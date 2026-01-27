b7c9e0b317b461f172e5a17bf3a8f8f002fa6faf
Kim <kim@naver.com>
2026-01-27 14:22:54 +0900

    평일 : my_django 폴더 내 코드작성

--- user/models.py
+++ user/models.py
@@ -1,11 +1,12 @@
-# from django.db import models
-# from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
 
+# Create your models here.
+class UserProfile(models.Model): # 유저 프로필 모델
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #1ㄷ1 관계
+    nickname = models.CharField("닉네임", max_length=15, blank=True)
+    img_profile = models.ImageField("프로필 이미지", upload_to="media/user", blank=True) # 프로필 이미지 , 필수입력 아님
+    description = models.TextField("소개글", blank=True)
 
-# class Profile(models.Model):
-#     user = models.OneToOneField() #user모델과 1대1로 연결(유저가 삭제되면 프로필도 삭제)
-#     image = models.ImageField(upload_to="user", blank=True, null=True)
-#     bio = models.TextField(blank=True)
-
-#     def __str__(self): 
-#         return f"{self.user.username}입니다."
+    def __str__(self):
+        return self.user.username
